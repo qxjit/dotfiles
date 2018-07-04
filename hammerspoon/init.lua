d698b70cff6c48553db3e1c236273fbf0dc1be60
David Vollbracht <david.vollbracht@gmail.com>
2018-07-04 08:51:41 -0400

    Adds hammerspoon bindings for moving windows among screens.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,12 +1,61 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "d", function()
-  switchLayoutToDvorak()
-  hs.alert.show(hs.keycodes.currentLayout())
+hotkeys = hs.hotkey.modal.new({"cmd"}, "h", "Hotkeys")
+
+-- Disable animations for window movements
+hs.window.animationDuration = 0
+
+hotkeys:bind({"cmd"}, "h", "Exit HotKeys", function()
+  hotkeys:exit()
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "u", function()
+hotkeys:bind({}, "d", function()
+  switchLayoutToDvorak()
+  hs.alert.show(hs.keycodes.currentLayout())
+  hotkeys:exit()
+end)
+
+hotkeys:bind({}, "u", function()
   switchLayoutToUS()
   hs.alert.show(hs.keycodes.currentLayout())
+  hotkeys:exit()
 end)
+
+hotkeys:bind({}, "f", function()
+  local win = hs.window.focusedWindow()
+  win:toggleFullScreen()
+  hotkeys:exit()
+end)
+
+hotkeys:bind({}, "Left", function()
+  local win = hs.window.focusedWindow()
+  savingAndRestoringFullscreen(win, function()
+    win:moveOneScreenWest()
+  end)
+  hotkeys:exit()
+end)
+
+hotkeys:bind({}, "Right", function()
+  local win = hs.window.focusedWindow()
+  savingAndRestoringFullscreen(win, function()
+    win:moveOneScreenEast()
+  end)
+  hotkeys:exit()
+end)
+
+function savingAndRestoringFullscreen(win, fn)
+  local wasFullscreen = win:isFullScreen()
+
+  if (wasFullscreen) then
+    win:setFullScreen(false)
+  end
+
+  fn()
+
+  if (wasFullscreen) then
+    hs.timer.doAfter(0.6, function()
+      win:setFullScreen(true)
+    end)
+  end
+end
 
 function switchLayoutToDvorak()
   hs.keycodes.setLayout("Dvorak")
